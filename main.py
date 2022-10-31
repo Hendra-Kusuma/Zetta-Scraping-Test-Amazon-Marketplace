35e1560f7531c73a0fa1079c02fcc5a977f8be17
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-10-31 22:45:17 +0700

    save update

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from bs4 import BeautifulSoup, soup
+from bs4 import BeautifulSoup
 import requests
 import os
 from playwright.sync_api import sync_playwright
-
-
 
 params = {
     'k': 'computer monitor',
@@ -14,9 +12,24 @@ params = {
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 '
                   'Safari/537.36 Edg/106.0.1370.52'}
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon'
+              '.com%2Fs%3Fk%3Dnokia%26crid%3D35KNQ4VMTGVTP%26sprefix%3Dnokia%252Caps%252C449%26ref%3Dnav_signin'
+              '&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle'
+              '=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+              '%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
+    page.fill('input#ap_email', 'ap.ms.em.yuni19ta@gmail.com')
+    page.click('input#continue')
+    page.fill('input#ap_password', 'abc12345@hk')
+    page.click('input#signInSubmit')
+    page.goto('https://www.amazon.com/s?k=computer+monitor&sprefix=compu%2Caps%2C451&ref=nb_sb_ss_pltr-ranker'
+              '-1hour_2_5')
+    page.is_visible('#search')
+    html = page.inner_html('#search')
 
 def Get_Total_Pages():
-
     params = {
         'k': 'computer monitor',
         'sprefix': 'compu,aps,451',
@@ -24,27 +37,76 @@ def Get_Total_Pages():
     }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24" }
+                      "Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24"}
 
     # Scraping Data
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=5)
-        page = browser.new_page()
-        # page.goto('https://www.amazon.com/s?k=computer+monitor&sprefix=compu%2Caps%2C451&ref=nb_sb_ss_pltr-ranker'
-        #           '-1hour_2_5')
-        page.goto('https://www.amazon.com/s?k=nokia&crid=35KNQ4VMTGVTP&sprefix=nokia%2Caps%2C449&ref=nb_sb_noss_1')
-        html = page.inner_html('#search')
+    soup = BeautifulSoup(html, 'html.parser')
+    total_pages = soup.find('span', 's-pagination-item s-pagination-disabled').text
+    return total_pages
 
-        soup = BeautifulSoup(html, 'html.parser')
-        total_pages = soup.find('span', 's-pagination-item s-pagination-disabled').text
-        return total_pages
 
 def Get_All_Item():
+    params = {
+        'k': 'computer monitor',
+        'sprefix': 'compu,aps,451',
+        'ref': 'nb_sb_ss_pltr-ranker-1hour_2_5'
+    }
+    soup = BeautifulSoup(html, 'html.parser')
+    # result = soup.find_all('div',"sg-col-inner")
 
-        check_titles = soup.find_all('div', 's-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16')
-        titles = soup.find_all('span', 'a-size-medium a-color-base a-text-normal')
-        for title in titles:
-            title = print(title.text)
+    title = soup.find_all('span', "a-size-medium a-color-base a-text-normal")
+    prices = soup.find_all('span', 'a-price-whole')
+    cents = soup.find_all('span', 'a-price-fraction')
+    stars = soup.find_all('span', 'a-icon-alt')
+    imagess = soup.find_all('div', 'a-section aok-relative s-image-fixed-height')
+    for images in imagess:
+        image = images.find('img')['src']
+
+
+
+    data_list = []
+
+    titles = soup.find_all('span', "a-size-medium a-color-base a-text-normal")
+    for titlesss in titles:
+        title = titlesss.text
+
+    prices = soup.find_all('span', 'a-price-whole')
+    for pricess in prices:
+        price = pricess.text
+
+    cents = soup.find_all('span', 'a-price-fraction')
+    for centss in cents:
+        cent = centss.text
+
+    stars = soup.find_all('span', 'a-icon-alt')
+    for starss in stars:
+        star = starss.text
+
+    imagess = soup.find_all('div', 'a-section aok-relative s-image-fixed-height')
+    for images in imagess:
+        image = images.find('img')['src']
+
+    # Sorting Data
+        data_dict = {
+            'title': title,
+            'cent': cent,
+            'price': price,
+            'image': image,
+            'star': star,
+        }
+
+        data_list.append(data_dict)
+        print(data_list)
+
+        # Write Json File
+        try:
+            os.mkdir('json_result')
+        except FileExistsError:
+            pass
+
+            with open(f'json_result/{query}_in_{location}_page_{counter}.json', 'w+') as json_data:
+                json.dump(job_list, json_data)
+        print(f'json page {counter} created')
 
 
 
@@ -60,10 +122,5 @@ def Get_All_Item():
     # Scraping Step
 
 
-
-
-
-
-
 if __name__ == '__main__':
-    Get_Total_Pages()
+    Get_All_Item()
